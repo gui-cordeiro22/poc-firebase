0b44cda0c3e0c86bd40a86d8894f3ee3b3e16b7a
Guilherme Cordeiro <guilhermecordeiro43@gmail.com>
2025-06-23 11:55:24 -0300

    feat: react-hook-form integration

--- src/pages/home-page/home.page.tsx
+++ src/pages/home-page/home.page.tsx
@@ -1,5 +1,6 @@
 // Dependencies
 import type { FunctionComponent } from "react";
+import { useForm } from "react-hook-form";
 
 // Components
 import { SignInForm } from "../../components/compositions/sign-in-form";
@@ -8,24 +9,48 @@ import { SignInForm } from "../../components/compositions/sign-in-form";
 import { HomePage } from "../../components/pages/home-page";
 
 export const Home: FunctionComponent = () => {
+  const { register, handleSubmit } = useForm();
+
+  const handleCreateUser = (data: any) => {
+    console.log(data);
+  };
+
   return (
     <>
       <HomePage
         siginFormSection={
           <SignInForm
-            handleClick={() => console.log("Enviando")}
-            nameInputCompositions={<input type="text" placeholder="Seu nome" />}
+            handleClick={handleSubmit(handleCreateUser)}
+            nameInputCompositions={
+              <input type="text" placeholder="Seu nome" {...register("name")} />
+            }
             emailInputCompositions={
-              <input type="email" placeholder="Seu e-mail" />
+              <input
+                type="email"
+                placeholder="Seu e-mail"
+                {...register("email")}
+              />
             }
             emailConfirmationInputCompositions={
-              <input type="email" placeholder="Confirmar e-mail" />
+              <input
+                type="email"
+                placeholder="Confirmar e-mail"
+                {...register("confirmationEmail")}
+              />
             }
-            phoneInputCompositions={<input type="text" placeholder="Celular" />}
+            phoneInputCompositions={
+              <input type="text" placeholder="Celular" {...register("phone")} />
+            }
             passwordInputCompositions={
-              <input type="password" placeholder="Sua senha" />
+              <input
+                type="password"
+                placeholder="Sua senha"
+                {...register("password")}
+              />
             }
-            acceptedTermsInputComposition={<input type="checkbox" />}
+            acceptedTermsInputComposition={
+              <input type="checkbox" {...register("acceptedTerms")} />
+            }
           />
         }
       />
